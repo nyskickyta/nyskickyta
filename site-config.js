6be799f6d78fdeb072c3cbce0a3a636e1d389b50
nyskickyta <info@nyskickyta.se>
2026-04-08 09:00:37 +0200

    Apply .gitignore cleanup

--- site-config.js
+++
@@ -1,5 +0,0 @@
-window.NYSKICK_SITE_CONFIG = {
-  googleMapsApiKey: "AIzaSyDuJVe5lEDhE_5w2FOBvOvt4E30qKpVtqk",
-  googleMapsAutocompleteCountry: "se",
-  quoteFormEndpoint: "https://admin.nyskickstenaltan.se/public-quote-request.php"
-};
